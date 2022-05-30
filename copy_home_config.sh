53190bb2701dc1808c519a4294fa53bd04b1bf63
Kyuunex <kyuunex@protonmail.ch>
2022-05-30 11:22:41 +0400

    add home copy script

---
+++ copy_home_config.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cp -avr ./home/username/.config ~/
+cp -avr ./home/username/.local ~/
+cp -avr ./home/username/.bash_profile ~/
+cp -avr ./home/username/.bashrc ~/
+cp -avr ./home/username/.profile ~/
+cp -avr ./home/username/.xinitrc ~/
+echo "copy done"
