a2a54462d749da02f1c0923fa4cd44278edfae8d
Kyuunex <kyuunex@protonmail.ch>
2023-05-12 19:28:51 +0400

    .initrc does not exist anymore, don't copy

--- copy_home_config.sh
+++ copy_home_config.sh
@@ -4,5 +4,4 @@ cp -avr ./home/username/.local ~/
 cp -avr ./home/username/.bash_profile ~/
 cp -avr ./home/username/.bashrc ~/
 cp -avr ./home/username/.profile ~/
-cp -avr ./home/username/.xinitrc ~/
 echo "copy done"
