0bb342dcd495e968907ebd08c2cae521d6ea5207
Kyuunex <kyuunex@protonmail.ch>
2023-05-16 17:38:34 +0400

    put sha check on multiple lines

--- copy_home_config.sh
+++ copy_home_config.sh
@@ -6,6 +6,9 @@ cp -avr ./home/username/.bashrc ~/
 cp -avr ./home/username/.profile ~/
 
 curl https://kyuunex.s-ul.eu/static/TLr1JPJY.png --create-dirs -o "$HOME/Pictures/wallpapers/large_ev414a.png"
-sha256sum "$HOME/Pictures/wallpapers/large_ev414a.png" | grep -q "d5ee70b867c95297ed90bb5d92b479d6a5bdc597149e4dab37e8d254a361f297" && echo "Wallpaper downloaded and its checksum verified" || echo "Wallpaper downloaded but its checksum failed!"
+
+sha256sum "$HOME/Pictures/wallpapers/large_ev414a.png" | 
+grep -q "d5ee70b867c95297ed90bb5d92b479d6a5bdc597149e4dab37e8d254a361f297" && 
+echo "Wallpaper downloaded and its checksum verified" || echo "Wallpaper downloaded but its checksum failed!"
 
 echo "copy done"
