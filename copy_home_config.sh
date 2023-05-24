b57b105a641d3eb207d02b955298694e785c9082
Kyuunex <kyuunex@protonmail.ch>
2023-05-24 08:05:42 +0400

    dl rofi theme

--- copy_home_config.sh
+++ copy_home_config.sh
@@ -11,4 +11,7 @@ sha256sum "$HOME/Pictures/wallpapers/large_ev414a.png" |
 grep -q "d5ee70b867c95297ed90bb5d92b479d6a5bdc597149e4dab37e8d254a361f297" && 
 echo "Wallpaper downloaded and its checksum verified" || echo "Wallpaper downloaded but its checksum failed!"
 
+curl https://raw.githubusercontent.com/undiabler/nord-rofi-theme/eebddcbf36052e140a9af7c86f1fbd88e31d2365/nord.rasi --create-dirs -o "$HOME/.config/rofi/nord.rasi"
+echo "rofi nord theme downloaded!"
+
 echo "copy done"
